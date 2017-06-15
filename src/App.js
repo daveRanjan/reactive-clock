1fa21fc245abe0fbbde7c1f58a39ee57b7604582
Dave Ranjan <dtiwari18@gmail.com>
2017-06-15 12:45:11 +0530

    Clock Showing local time

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import PrimeClock from './components/PrimeClock';
 import SideMenu from './components/SideMenu';
 import './App.css'
+import moment from 'moment';
 class App extends React.Component {
 
     render() {
@@ -9,7 +10,7 @@ class App extends React.Component {
             <div id="outer-container">
                 <SideMenu  pageWrapId={ "page-wrap" } outerContainerId={ "outer-container" }/>
                 <main id="page-wrap">
-                    <PrimeClock timezone="America/Thule"/>
+                    <PrimeClock timezone={moment.tz.guess()}/>
                 </main>
             </div>
         );
---
+++ src/components/SmallClock.js
@@ -0,0 +1,62 @@
+import React from 'react';
+import {convertTimezoneToReadable} from '../util/TimeUtil';
+import Moment from 'react-moment';
+import moment from 'moment';
+import '../style/SmallClock.css';
+
+class SmallClock extends React.Component {
+
+    constructor(props){
+        super(props);
+        this.timezone = this.props.timezone;
+        let currentTime = new moment().tz(this.timezone);
+        this.state = {
+            moment_date : currentTime,
+            timezone_readable : convertTimezoneToReadable(this.timezone),
+            timestamp : currentTime.unix()
+        };
+
+        setInterval(() => {
+            let currentTime = new moment().tz(this.timezone);
+            this.setState({moment_date : currentTime});
+        }, 1000);
+    }
+
+    render(){
+        return (<div className="widget">
+            <div className="widget-top-section">
+                <h1>{this.state.timezone_readable}</h1>
+            </div>
+            <div className="timer">
+                <div className="timer-session">
+                    <div className="timer-session-log">
+                        <p className="subtitle alpha"><Moment format="dddd, DD MMM YYYY">{this.state.moment_date}</Moment></p>
+                    </div>
+                </div>
+                <div className="timer-tick">
+                    <div className="timer-digits">
+                        <div className="timer-digit-group">
+                            <div className="timer-digit">{this.state.moment_date.format('HH')}</div>
+                            <div className="timer-colon">:</div>
+                            <p className="timer-unit alpha">HOURS</p>
+                        </div>
+                        <div className="timer-digit-group">
+                            <div className="timer-digit">{this.state.moment_date.format('mm')}</div>
+                            <div className="timer-colon">:</div>
+                            <p className="timer-unit alpha">MINUTES</p>
+                        </div>
+                        <div className="timer-digit-group">
+                            <div className="timer-digit">{this.state.moment_date.format('ss')}</div>
+                            <p className="timer-unit alpha">SECONDS</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>);
+    }
+
+}
+
+export default PrimeClock;
+
+
